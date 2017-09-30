b7b51b59c5526c046d8cb9ff6267e110166ee4ee
Harry-3d <3d2dme@gmail.com>
2017-09-30 11:17:25 +1000

    player control

---
+++ TheGame/src/mygame/LoadScene.java
@@ -0,0 +1,24 @@
+
+package mygame;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+
+/*
+ * Load a .j3o scene and any required models
+ * @author Harry-3d
+ */
+
+public class LoadScene {
+    
+    AssetManager assetManager;
+    LoadScene(AssetManager assMgr){
+        assetManager = assMgr;  
+    }  
+}
---
+++ TheGame/src/mygame/OurPlayer.java
@@ -0,0 +1,106 @@
+package mygame;
+
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.audio.Listener;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.input.FlyByCamera;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import com.jme3.renderer.ViewPort;
+
+/**
+ * @author normen
+ * @author Harry-3d
+ */
+
+public class OurPlayer implements ActionListener{
+    private CharacterControl player;
+    private boolean left = false, right = false, up = false, down = false;
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
+    private Vector3f walkDirection = new Vector3f();
+  
+    // Create our player
+    public CharacterControl initPlayer(ViewPort viewPort, FlyByCamera flyCam){
+        viewPort.setBackgroundColor(new ColorRGBA(.1f, 1f, 1f, 1f));
+        flyCam.setMoveSpeed(100);
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+        player.setJumpSpeed(20);
+        player.setFallSpeed(30);
+        player.setGravity(30);
+        player.setPhysicsLocation(new Vector3f(0, 15, 20));
+        return player;  
+    }
+    
+    // Set up WASD, Space to jump
+    public void setUpKeys(InputManager inputManager) {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Jump");
+    }
+    
+    /** These are our custom actions triggered by key presses.
+    * We do not walk yet, we just keep track of the direction the user pressed. */
+      @Override
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        if (binding.equals("Left")) {
+          left = isPressed;
+        } else if (binding.equals("Right")) {
+          right= isPressed;
+        } else if (binding.equals("Up")) {
+          up = isPressed;
+        } else if (binding.equals("Down")) {
+          down = isPressed;
+        } else if (binding.equals("Jump")) {
+          if (isPressed) { player.jump(); }
+        }
+    }
+  
+    /**
+   * This is the main event loop--walking happens here.
+   * We check in which direction the player is walking by interpreting
+   * the camera direction forward (camDir) and to the side (camLeft).
+   * The setWalkDirection() command is what lets a physics-controlled player walk.
+   * We also make sure here that the camera moves with player.
+   */
+  
+    public void simpleUpdate(float tpf, Camera cam, Listener listener) {
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
+        
+        // Move the listener with the camera
+        listener.setLocation(cam.getLocation());
+        listener.setRotation(cam.getRotation());
+        
+    }
+  
+}
+
